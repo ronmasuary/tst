97e6bbebec0ebd13dd2cefea903c06a3f1747d6e
Ron Masuary <ronmas2@gmail.com>
2020-07-21 20:18:13 +0300

    Updates

---
+++ precache-manifest.884ce38619767fef48561e8d6d66aa21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213ebcc45469f64e281616a03054b159",
+    "url": "/tst/index.html"
+  },
+  {
+    "revision": "69225ad5212c3fd073be",
+    "url": "/tst/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84f1b204088f9eef04a9",
+    "url": "/tst/static/js/2.8b561341.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tst/static/js/2.8b561341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69225ad5212c3fd073be",
+    "url": "/tst/static/js/main.55a8e99d.chunk.js"
+  },
+  {
+    "revision": "de959ff2a50503e02a29",
+    "url": "/tst/static/js/runtime-main.62e2247a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tst/static/media/logo.5d5d9eef.svg"
+  }
+]);
